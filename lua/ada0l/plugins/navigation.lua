063709a125a6d9dfd30fe0f3b37b1025f065581d
ada0l <andreika.varfolomeev@yandex.ru>
2023-07-23 04:16:59 +1000

    update

--- lua/ada0l/plugins/navigation.lua
+++ lua/ada0l/plugins/navigation.lua
@@ -9,6 +9,13 @@ return {
         end,
         desc = 'Files Toggle',
       },
+      {
+        '<leader>N',
+        function ()
+          require('mini.files').open(vim.api.nvim_buf_get_name(0))
+        end,
+        desc = 'Files Toggle',
+      }
     },
     opts = {
       mappings = {
